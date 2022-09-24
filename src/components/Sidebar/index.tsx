635690f65cc133fe7957de300186fb3370191e47
Tom Stannett <drainzine@gmail.com>
2022-09-24 17:05:46 +0100

    content update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { Container, Row, Text } from "@nextui-org/react";
 import ScrollableLine from "@components/ScrollableLine";
 import { useIsTablet } from "@hooks/useMediaQuery";
 import { Divider } from "./styling";
+import { content } from "@data/content";
 
 const Sidebar = () => {
   const isTabletPortrait = useIsTablet(false, false);
@@ -14,41 +15,24 @@ const Sidebar = () => {
             Playlist of Babel
           </Text>
         </Row>
-        {/* <div
-          style={{
-            width: "100%",
-            height: 2,
-            background: "var(--nextui-colors-text)",
-            marginLeft: "-3rem",
-          }}
-        ></div> */}
         <Divider/>
-        <Row css={{ marginTop: "$5" }}>
-          <Text size={16}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non
-            tempus ante, eu ullamcorper lorem. Morbi eget ligula eros. Vivamus
-            viverra ligula sed maximus scelerisque. Proin leo felis, commodo quis
-            auctor ut, laoreet eget dui. Maecenas vestibulum condimentum
-            condimentum. Proin egestas viverra urna nec blandit. Etiam condimentum
-            felis ut maximus varius. Curabitur lobortis porttitor justo, vitae
-            malesuada turpis porta sed. Mauris ullamcorper tincidunt euismod.
-            Integer metus felis, consectetur id tortor id, finibus consectetur
-            sapien. Morbi rhoncus ac nunc ut egestas.
+          {content.sidebarMain.map((paragraph: string) =>
+            <Row css={{ marginTop: "$5" }}>
+            <Text size={16}>
+              {paragraph}
+            </Text>
+          </Row>
+          )}
+          {!!content?.sidebarFooter &&
+          <Row css={{ marginTop: "$10" }}>
+          <Text css={{
+            fontStyle: 'italic'
+          }} size={12}>
+            {content.sidebarFooter}
           </Text>
         </Row>
-        <Row css={{ marginTop: "$5" }}>
-          <Text size={16}>
-            Sed varius tellus non libero malesuada tempus. In id neque ultricies,
-            ullamcorper ipsum ullamcorper, porttitor lectus. Sed dictum congue
-            tortor. Nullam varius egestas rutrum. Nam dictum sagittis augue, vel
-            imperdiet ex rutrum vitae. Donec purus eros, commodo in est ac,
-            malesuada lacinia turpis. Mauris maximus gravida nisl, vitae ultrices
-            libero condimentum id. Aliquam faucibus aliquam fringilla. Curabitur
-            quis ultricies dui. Ut id nisi tristique turpis sagittis molestie nec
-            non mi. Vivamus et porttitor augue. Cras ultrices vulputate nulla, id
-            ultrices risus. Phasellus a dolor ex.
-          </Text>
-        </Row>
+          }
+
       </Container>
     );
 };
